6034fcb6bf9d903df7c8b72227411801548d1613
dijayst <dijayst@gmail.com>
2022-12-23 02:15:45 +0100

    complete

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState,useEffect } from 'react'
-import {  Button, FlatList, Image, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import {  Alert, Button, FlatList, Image, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import Hy from './gamescomponent/Hy';
 import LinearGradient from 'react-native-linear-gradient';
 
@@ -7,102 +7,71 @@ import LinearGradient from 'react-native-linear-gradient';
 
 //const AuthStack= createStackNavigator();
 export default function App() {
-  const [name1, setname1] = useState([
-    {name: 'ebun ', id:'1'},
-    {name: 'dushen', id:'2'},
-    {name: 'sally', id:'3'},
-    {name: 'dami', id:'4'},
-    {name: 'ayo', id:5},
-    {name: 'ebun ', id:'6'},
-    {name: 'dushen', id:'7'},
-    {name: 'sally', id:'8'},
-    {name: 'dami', id:'9'}
-  ]);
- 
-const total=3;
+  
+
+
+const [handle,sethandle]=useState(false);
+
+const imagee=[{name:"select the flag of Nigeria",options:[
+  {image:require("./imagee/uk.png"),iscorrect:false},
+  {image:require("./imagee/nig.png"),iscorrect:true},
+  {image:require("./imagee/gh.png"),iscorrect:false}]},
+  
+  {name:"select the flag of Us",options:[
+  {image:require("./imagee/us.webp"),iscorrect:true},
+  {image:require("./imagee/nig.png"),iscorrect:false},
+{image:require("./imagee/russia.jpg"),iscorrect:false}]},
+
+{name:"select the flag of russia",options:[
+  {image:require("./imagee/us.webp"),iscorrect:false},
+  {image:require("./imagee/nig.png"),iscorrect:false},
+{image:require("./imagee/russia.jpg"),iscorrect:true}]}]
+             
 
 const [score,setscore]=useState(0)
-const handle=false;
+ const [imageeIndex,setimageeIndex]=useState(0)
+ const handleindex=(iscorrect)=>{
+   if(iscorrect===true){
+setscore(score+1)
+console.log("correct answer")
 
-const imagee=[{id:"1",image:require("./imagee/uk.png")},
-              {id:"2",image:require("./imagee/nig.png")},
-              {id:"3",image:require("./imagee/gh.png")}];
-const image2=[{id:"1",image:require("./imagee/nig.png")},
-             {id:"2",image:require("./imagee/uk.png")},
-             {id:"3",image3:require("./imagee/uk.png")}]
-
-  const pressHandle=()=>{  
-      //navigation.navigate('Hy')
- // navigation.push('Contact')
-}
-
-             /*
-             const nextquest=()=>{
-              imagee.filter((item)=>{return(
-              if(item.image !=handlequest||item.image===""){
-                alert("you missed question one")
-              
-              }
-              )
-            })
-             }*/
-      const anwser=[imagee[1],"cat"]
-           //  console.log(anwser[0])
-    const handlequest=(id)=>{
-     let arr=imagee.filter((item)=>{
-       if(item.id!=id){
-       setscore(score+1)
-     }
-
-  }) 
-  console.log(arr);
-   // setscore(score+1)
-  //  if(id===anwser[0]){  setscore(score+1)}
-   // return handle
-console.log(id)
-//console.log(imagee[1])
-     // e.preventDefault();
-      //console.log(e.target.entry)
-     //   item.image==image)
-//console.log(event.target)
-//console.log(imagee.findIndex(item))
-  // console.log(index)
-   //alert("hey")
-     
+   } else{
+    console.log("wrong answer")
+    imagee
     }
-
-const handlename=(id)=>{
+    
+   const nextquestion=imageeIndex+1
+  if(nextquestion<imagee.length){
+    setimageeIndex(nextquestion)
+  }else{
+    alert( + score+1)
+    setimageeIndex(imageeIndex)
+  }
   
-  setname1((prevname)=>{
-    return prevname.find(person=>{ if(person.name==="dami"){
-      console.log("yeah")
-    }else{console.log("cant find")}})
-  })
-  //alert("yyeah")
-  console.log(name1)
-}
+  
+  }
 
               return (
               
    <View style={styles.container}>
-     
-     <Text  style={{color:"grey",marginTop:65,fontSize:30,alignSelf:"center"}}>Guess the country</Text>
-       <View style={{backgroundColor:"#fffffe",opacity:.6,alignItems:"center",marginTop:65,height:300,borderRadius:5,}}>
-       <Text style={{color:"black",fontSize:20,fontWeight:"bold"}}>
-         Nigeria
-       </Text>
+     <Text  style={{color:"black",marginTop:65,fontWeight:"bold",fontSize:30,alignSelf:"center"}}>Guess the country</Text>
+       <View style={{backgroundColor:"coral",opacity:.6,alignItems:"center",marginTop:60,height:319,borderRadius:5,}}>
       
       {/* <Hy  handle={handle} handl={handlequest} ques={imagee}/>
       
        <Button title='click me' onPress={()=>{pressHandle()}} />
       */}
-      {imagee.map((item,index)=>{return(
-          <TouchableOpacity key={item.id} onPress={()=>handlequest(item.id)} style={styles.typo}>
+      
+      <Text style={{marginLeft:20}}>question:{imageeIndex+1}/{imagee.length}</Text>
+ 
+      <Text style={{color:"black",fontSize:20}}>{imagee[imageeIndex].name}</Text>
+      {imagee[imageeIndex].options.map((item,index)=>{return(
+          <TouchableOpacity key={item} onPress={()=>handleindex(item.iscorrect)} style={styles.typo}>
           <Image source={item.image} style={styles.imagearray} />
           </TouchableOpacity>)})}
        </View>
-        <Text style={{color:"grey",fontSize:20}}>Score: {score}</Text>
- 
+        <Text style={{color:"black",fontSize:20}}>Score: {score}</Text>
+       
       
       </View>
   )
@@ -111,9 +80,10 @@ const handlename=(id)=>{
 
 const styles=StyleSheet.create({
   container:{
-    backgroundColor:'yellow',
+    backgroundColor:'white',
     flex:1,
-  paddingHorizontal:20
+  paddingHorizontal:20,
+  color:"black",
   },
 
   body:{
